4d6e3ae1662b1678c1a84440aaa66dd76853bba9
Philipp <philipp.niemetz@gmail.com>
2022-03-23 10:20:39 +0100

    init commit

---
+++ BuilderPersons/BuilderPersons/Person.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace BuilderPersons
+{
+    public class Person
+    {
+        private readonly string firstname;
+        private readonly string lastname;
+        private readonly int age;
+        private readonly string phone;
+        private readonly string address;
+
+        public string Firstname => firstname;
+        public string Lastname => lastname;
+        public int Age => age;
+        public string Phone => phone;
+        public string Address => address;
+
+        private Person(Builder builder)
+        {
+            this.firstname = builder.firstname;
+            this.lastname = builder.lastname;
+            this.age = builder.age;
+            this.phone = builder.phone;
+            this.address = builder.address;
+        }
+
+        public class Builder
+        {
+            internal readonly string firstname;
+            internal readonly string lastname;
+            internal int age;
+            internal string phone;
+            internal string address;
+
+            public Builder(string firstname, string lastname)
+            {
+                this.firstname = firstname;
+                this.lastname = lastname;
+            }
+
+            public Builder Age(int age)
+            {
+                this.age = age;
+                return this;
+            }
+
+            public Builder Phone(string phone)
+            {
+                this.phone = phone;
+                return this;
+            }
+
+            public Builder Address(string address)
+            {
+                this.address = address;
+                return this;
+            }
+
+            public Person Build()
+            {
+                var person = new Person(this);
+                if (person.age > 115) throw new InvalidOperationException("Person is too old");
+                if (firstname.Length < 3) throw new InvalidOperationException("Firstname is too short");
+                return person;
+            }
+        }
+    }
+}
---
+++ BuilderPersons/BuilderPersons/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+namespace BuilderPersons
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            ReadCsv();
+        }
+
+        private static void ReadCsv()
+        {
+            var reader = new StreamReader("persons_with_address.csv");
+            while (!reader.EndOfStream)
+            {
+                var line = reader.ReadLine();
+                if (line == null) continue;
+                var parts = line.Split(';');
+                try
+                {
+                    var person = new Person.Builder(parts[0], parts[1]).Age(int.Parse(parts[2])).Phone(parts[3]).Address(parts[4]).Build();
+                }
+                catch (Exception ex)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(line);
+                }
+            }
+            Console.ForegroundColor = ConsoleColor.Black;
+        }
+    }
+}
